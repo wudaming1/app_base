934286344611dfcef9df89ae6c429fba3086cade
daming.wu <daming.wu@carsome.com>
2023-09-08 18:18:49 +0800

    add my sliver

--- example/lib/pages/sliver_architecture/sliver_architecture_controller.dart
+++ example/lib/pages/sliver_architecture/sliver_architecture_controller.dart
@@ -3,4 +3,11 @@ import 'package:app_base/app_base.dart';
 
 class SliverArchitectureController extends BaseController{
 
+  double height = 100;
+
+  void updateHeight(){
+    height = height + 50;
+    update();
+  }
+
 }
--- example/lib/pages/sliver_architecture/sliver_architecture_page.dart
+++ example/lib/pages/sliver_architecture/sliver_architecture_page.dart
@@ -1,5 +1,7 @@
 import 'package:app_base/app_base.dart';
 import 'package:example/pages/sliver_architecture/sliver_architecture_controller.dart';
+import 'package:example/widgets/my_sliver.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class SliverArchitecturePage extends BasePage<SliverArchitectureController> {
@@ -9,12 +11,41 @@ class SliverArchitecturePage extends BasePage<SliverArchitectureController> {
 
   @override
   Widget buildPage(BuildContext context) {
-    return ListView.builder(
-      itemCount: 10,
-      itemBuilder: (BuildContext context, int index) {
-        return Container(
-            padding: const EdgeInsets.all(16), child: Text(index.toString()));
-      },
+    return CustomScrollView(
+      // scrollDirection: Axis.horizontal,
+      // reverse: true,
+      slivers: [
+        SliverToBoxAdapter(
+          child: OutlinedButton(
+            onPressed: controller.updateHeight,
+            child: Container(
+              color: Colors.yellow,
+              margin: EdgeInsets.all(16),
+              height: controller.height,
+              width: 100,
+            ),
+          ),
+        ),
+        // MySliver(100,
+        //     child: ColoredBox(
+        //       color: Colors.red.withOpacity(0.5),
+        //     )),
+
+        ...List.generate(
+            40,
+            (index) => SliverToBoxAdapter(
+                  child: Container(
+                    color: Colors.yellow,
+                    margin: EdgeInsets.all(16),
+                    height: 100,
+                    width: 100,
+                  ),
+                )),
+        MySliver(100,
+            child: ColoredBox(
+              color: Colors.green.withOpacity(0.5),
+            )),
+      ],
     );
   }
 
